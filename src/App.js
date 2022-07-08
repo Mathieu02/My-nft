1d28904b26882cca1829253f4ca1ae5d13a7a876
mathieu duval <mathieu26101999@gmail.com>
2022-07-08 15:08:21 +0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,188 @@
+import { useEffect, useState } from "react";
+import {
+  VStack,
+  useDisclosure,
+  Button,
+  Text,
+  HStack,
+  Select,
+  Input,
+  Box
+} from "@chakra-ui/react";
+import SelectWalletModal from "./Modal";
+import { useWeb3React } from "@web3-react/core";
+import { CheckCircleIcon, WarningIcon } from "@chakra-ui/icons";
+import { Tooltip } from "@chakra-ui/react";
+import { networkParams } from "./networks";
+import { connectors } from "./connectors";
+import { toHex, truncateAddress } from "./utils";
+
+export default function Home() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const {
+    library,
+    chainId,
+    account,
+    activate,
+    deactivate,
+    active
+  } = useWeb3React();
+  const [signature, setSignature] = useState("");
+  const [error, setError] = useState("");
+  const [network, setNetwork] = useState(undefined);
+  const [message, setMessage] = useState("");
+  const [signedMessage, setSignedMessage] = useState("");
+  const [verified, setVerified] = useState();
+
+  const handleNetwork = (e) => {
+    const id = e.target.value;
+    setNetwork(Number(id));
+  };
+
+  const handleInput = (e) => {
+    const msg = e.target.value;
+    setMessage(msg);
+  };
+
+  const switchNetwork = async () => {
+    try {
+      await library.provider.request({
+        method: "wallet_switchEthereumChain",
+        params: [{ chainId: toHex(network) }]
+      });
+    } catch (switchError) {
+      if (switchError.code === 4902) {
+        try {
+          await library.provider.request({
+            method: "wallet_addEthereumChain",
+            params: [networkParams[toHex(network)]]
+          });
+        } catch (error) {
+          setError(error);
+        }
+      }
+    }
+  };
+
+  const signMessage = async () => {
+    if (!library) return;
+    try {
+      const signature = await library.provider.request({
+        method: "personal_sign",
+        params: [message, account]
+      });
+      setSignedMessage(message);
+      setSignature(signature);
+    } catch (error) {
+      setError(error);
+    }
+  };
+
+  const verifyMessage = async () => {
+    if (!library) return;
+    try {
+      const verify = await library.provider.request({
+        method: "personal_ecRecover",
+        params: [signedMessage, signature]
+      });
+      setVerified(verify === account.toLowerCase());
+    } catch (error) {
+      setError(error);
+    }
+  };
+
+  const refreshState = () => {
+    window.localStorage.setItem("provider", undefined);
+    setNetwork("");
+    setMessage("");
+    setSignature("");
+    setVerified(undefined);
+  };
+
+  const disconnect = () => {
+    refreshState();
+    deactivate();
+  };
+
+  useEffect(() => {
+    const provider = window.localStorage.getItem("provider");
+    if (provider) activate(connectors[provider]);
+  }, []);
+
+  return (
+    <>
+      
+      <VStack justifyContent="center" alignItems="center" h="100vh">
+        <HStack marginBottom="10px">
+          <Text
+            margin="0"
+            lineHeight="1.15"
+            fontSize={["1.5em", "2em", "3em", "4em"]}
+            fontWeight="600"
+          >
+           Connect to
+          </Text>
+          <Text
+            margin="0"
+            lineHeight="1.15"
+            fontSize={["1.5em", "2em", "3em", "4em"]}
+            fontWeight="600"
+            sx={{
+              background: "linear-gradient(90deg, #1652f0 0%, #b9cbfb 70.35%)",
+              WebkitBackgroundClip: "text",
+              WebkitTextFillColor: "transparent"
+            }}
+          >
+            Unlock your 3D model
+          </Text>
+        </HStack>
+        <HStack>
+          {!active ? (
+            <Button onClick={onOpen}>Connect Wallet</Button>
+          ) : (
+            <Button onClick={disconnect}>Disconnect</Button>
+          )}
+        </HStack>
+        <VStack justifyContent="center" alignItems="center" padding="10px 0">
+          <HStack>
+            <Text>{`Connection Status: `}</Text>
+            {active ? (
+              <CheckCircleIcon color="green" />
+            ) : (
+              <WarningIcon color="#cd5700" />
+            )}
+          </HStack>
+
+          
+          
+        </VStack>
+        {active && (
+          <HStack justifyContent="flex-start" alignItems="flex-start">
+            
+            <Box
+              maxW="sm"
+              
+              borderRadius="lg"
+              
+              padding="10px"
+            >
+              <VStack>
+              <a href="https://google.com" target="_blank" rel="noreferrer">
+                <Button>
+                https://google.com
+                </Button>
+                </a>
+                
+              </VStack>
+            </Box>
+            
+               
+           
+          </HStack>
+        )}
+        <Text>{error ? error.message : null}</Text>
+      </VStack>
+      <SelectWalletModal isOpen={isOpen} closeModal={onClose} />
+    </>
+  );
+}
